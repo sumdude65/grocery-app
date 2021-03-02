c2e01d9ec1fee5f8b7411b79bdb9569bda9551a3
sumdude65 <imadeosaretin@gmail.com>
2021-03-02 08:34:02 +0100

    working calculator
    still a bug where multiple periods can be in one number

--- src/App.js
+++ src/App.js
@@ -1,43 +1,101 @@
 import React, { useState } from "react";
 import "./style.scss";
-import { text } from "./text";
-const marked = require("marked");
-marked.setOptions({
-  breaks: true,
-});
+// import mathjs library
+import { create, all } from "mathjs";
+const config = {};
+
+const math = create(all, config);
 
 export const App = () => {
-  const [value, setValue] = useState(text);
-  const handleChange = (event) => {
-    setValue(event.target.value);
+  const [display, setDisplay] = useState("");
+
+  //handles button presses
+  const handleClick = (i) => {
+    const displayCopy = display.toString().slice();
+
+    //tests for consecutive periods
+    const regex = /\.\./g;
+
+    //checks for invalid math sign placement like */ etc
+    const regex2 = /\+\+|\+\/|\+\*|-\/|-\*|-\+|\*\/|\/\*/g;
+    if (regex.test(displayCopy.concat(i))) {
+      return;
+    }
+    if (regex2.test(displayCopy.concat(i))) {
+      return;
+    }
+
+    //prevents a number starting with multiple zeroes
+    if (displayCopy === "0" && i === 0) {
+      return;
+    }
+    //changes the 0 to the input number when a new calculation is started
+    if (displayCopy === "0" && i !== 0) {
+      setDisplay(i.toString());
+      return;
+    }
+
+    setDisplay(displayCopy.concat(i));
   };
-  const markedup = () => {
-    return {
-      __html: marked(value),
-    };
+
+  // handles calculation
+  const handleSubmit = () => {
+    const displayCopy = display.slice();
+    setDisplay(math.evaluate(displayCopy).toString());
+  };
+
+  const handleClear = () => {
+    setDisplay("0");
   };
   return (
-    <div className="app">
-      <div id="EDITOR" className="editor">
-        <h1>Editor</h1>
-        <textarea
-          name="text"
-          id=""
-          cols="40"
-          rows="20"
-          value={value}
-          onChange={handleChange}
-          id="editor"
-        ></textarea>
+    <div className="calculator">
+      <Display id="display" display={display} />
+      <div className="row">
+        <Button id="clear" value="AC" handleClick={() => handleClear()} />
+        <Button id="divide" value="/" handleClick={() => handleClick("/")} />
       </div>
-      <div className="preview">
-        <h1 className="h1">Preview</h1>
-        <div id="preview" dangerouslySetInnerHTML={markedup()}></div>
+      <div className="row">
+        <Button id="seven" value="7" handleClick={() => handleClick(7)} />
+        <Button id="eight" value="8" handleClick={() => handleClick(8)} />
+        <Button id="nine" value="9" handleClick={() => handleClick(9)} />
+        <Button id="multiply" value="*" handleClick={() => handleClick("*")} />
       </div>
-      <footer>
-        Made with love by
-        <a href="https://twitter.com/imadeosaretin_"> Imade Osaretin Frank</a>
-      </footer>
+      <div className="row">
+        <Button id="four" value="4" handleClick={() => handleClick(4)} />
+        <Button id="five" value="5" handleClick={() => handleClick(5)} />
+        <Button id="six" value="6" handleClick={() => handleClick(6)} />
+        <Button id="subtract" value="-" handleClick={() => handleClick("-")} />
+      </div>
+
+      <div className="row">
+        <Button id="one" value="1" handleClick={() => handleClick(1)} />
+        <Button id="two" value="2" handleClick={() => handleClick(2)} />
+        <Button id="three" value="3" handleClick={() => handleClick(3)} />
+        <Button id="add" value="+" handleClick={() => handleClick("+")} />
+      </div>
+      <div className="row">
+        <Button id="zero" value="0" handleClick={() => handleClick(0)} />
+        <Button id="decimal" value="." handleClick={() => handleClick(".")} />
+        <Button id="equals" value="=" handleClick={() => handleSubmit()} />
+      </div>
     </div>
+  );
+};
+
+const Display = (props) => {
+  return (
+    <div id={props.id} className="display">
+      <h1>{props.display}</h1>
+    </div>
+  );
+};
+
+const Button = (props) => {
+  const value = props.value;
+
+  return (
+    <button id={props.id} onClick={() => props.handleClick()}>
+      {value}
+    </button>
   );
 };
