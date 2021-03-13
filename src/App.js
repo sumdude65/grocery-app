c672dd7c62f7ac1cc3a3e136ca47d21b10633d25
sumdude65 <imadeosaretin@gmail.com>
2021-03-13 20:39:44 +0100

    working grocery app
    completed, added all features

--- src/App.js
+++ src/App.js
@@ -1,61 +1,105 @@
-import React, { useState, useEffect, useRef } from "react";
-import logo from "./components/logo.svg";
-import { links, socials } from "./components/data";
-import { FaBars } from "react-icons/fa";
+import React, { useState, useEffect } from "react";
+import { BiEdit, BiSave, BiTrash } from "react-icons/bi";
 import "./style.scss";
 
-export const Navbar = () => {
-  const [openNav, setOpenNav] = useState(false);
-  const linksContainer = useRef(null);
-  const linksRef = useRef(null);
-
-  const toggleNav = () => {
-    setOpenNav(!openNav);
-  };
-
+export const App = () => {
+  const [input, setInput] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editIndex, setEditIndex] = useState(null);
+  // checks if a list already exists in local storage
   useEffect(() => {
-    if (openNav) {
-      const containerHeight = linksRef.current.getBoundingClientRect().height;
-      linksContainer.current.style.height = containerHeight + "px";
-    } else {
-      linksContainer.current.style.height = "0px";
-    }
-  });
-  useEffect(() => {
-    if (window.innerWidth > 850) {
-      console.log("setted");
-      setOpenNav(true);
+    const storageAvailable = JSON.parse(localStorage.getItem("list"));
+    if (storageAvailable) {
+      setList(storageAvailable);
     }
   }, []);
-
+  // both functions handle editing phase
+  const editing = (id) => {
+    const newInput = list.find((item) => item.id === id);
+    const inputIndex = list.indexOf(newInput);
+    setInput(newInput);
+    setIsEditing(true);
+    setEditIndex(inputIndex);
+  };
+  const finishedEditing = (e) => {
+    e.preventDefault();
+    const newList = [...list].map((item, idx) => {
+      if (idx === editIndex) {
+        item.message = input.message;
+      }
+      return item;
+    });
+    setList(newList);
+    localStorage.setItem("list", JSON.stringify(newList));
+    setInput("");
+    setIsEditing(false);
+    setEditIndex(null);
+  };
+  //handles text input
+  const handleChange = (e) => {
+    if (isEditing) {
+      setInput({ ...input, message: e.target.value });
+    } else {
+      setInput({
+        id: Date.now(),
+        message: e.target.value,
+      });
+    }
+  };
+  //updates list and local storage
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (input.message) {
+      setList([...list, input]);
+      localStorage.setItem("list", JSON.stringify([...list, input]));
+    } else {
+      alert("Add item");
+      return;
+    }
+    setInput("");
+  };
+  // remove item
+  const remove = (id) => {
+    const newList = list.filter((item) => item.id !== id);
+    setList(newList);
+    localStorage.setItem("list", JSON.stringify(newList));
+  };
   return (
-    <nav>
-      <div className="logo">
-        <img src={logo} alt="logo" />
-        <button id="navbar-toggle" onClick={() => toggleNav()}>
-          <FaBars />
-        </button>
-      </div>
-      <div className="linksContainer" ref={linksContainer}>
-        <ul className="list" ref={linksRef}>
-          {links.map((link) => {
-            return (
-              <li key={link.id}>
-                <a href={link.url}>{link.title}</a>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-      <div className="socials">
-        {socials.map((social, idx) => {
+    <div className="app">
+      <h1>Grocery App</h1>
+      <form>
+        <input
+          type="text"
+          onChange={handleChange}
+          value={input.message || ""}
+        />
+        {!isEditing ? (
+          <button onClick={handleSubmit}>Submit</button>
+        ) : (
+          <button onClick={finishedEditing}>
+            Save <BiSave />
+          </button>
+        )}
+      </form>
+
+      <div className="list">
+        {list.map(({ id, message }) => {
           return (
-            <a key={idx} href={social.url}>
-              {social.icon}
-            </a>
+            <li key={id}>
+              <p>{message}</p>
+              <div className="buttons">
+                <button onClick={() => remove(id)}>
+                  <BiTrash />
+                </button>
+                <button onClick={() => editing(id)}>
+                  <BiEdit />
+                </button>
+              </div>
+            </li>
           );
         })}
       </div>
-    </nav>
+    </div>
   );
 };
